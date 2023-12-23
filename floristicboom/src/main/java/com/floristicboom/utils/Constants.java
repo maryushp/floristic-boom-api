f80cf040b7fcc00fdaac01491f4237313f3eb786
MfalconP <maryushp@gmail.com>
2023-12-23 17:55:32 +0100

    feat(delivery-type) updated entity dto mapper and added more constants

--- floristicboom/src/main/java/com/floristicboom/utils/Constants.java
+++ floristicboom/src/main/java/com/floristicboom/utils/Constants.java
@@ -7,7 +7,9 @@ public class Constants {
     public static final String FLOWER_NOT_FOUND = "Flower with id %d not found";
     public static final String NO_SUCH_ITEM_ERROR = "No Such Item";
     public static final String ALREADY_EXIST_ERROR = "Item Already Exist";
-    public static final String FLOWER_ALREADY_EXISTS = "Flower with name already exists";
+    public static final String FLOWER_ALREADY_EXISTS = "Flower already exists";
+    public static final String DELIVERY_TYPE_ALREADY_EXISTS = "Delivery type with name %s already exists";
+    public static final String DELIVERY_TYPE_NOT_FOUND = "Delivery type with id %d not found";
     public static final String IMAGE_URI = "image_uri";
     public static final String AVAILABLE_QUANTITY = "availableQuantity";
     public static final String NAME = "name";
--- floristicboom/src/main/java/com/floristicboom/utils/mappers/EntityToDtoMapper.java
+++ floristicboom/src/main/java/com/floristicboom/utils/mappers/EntityToDtoMapper.java
@@ -1,5 +1,7 @@
 package com.floristicboom.utils.mappers;
 
+import com.floristicboom.delivery.type.model.DeliveryType;
+import com.floristicboom.delivery.type.model.DeliveryTypeDTO;
 import com.floristicboom.flower.model.Flower;
 import com.floristicboom.flower.model.FlowerDTO;
 import org.mapstruct.Mapper;
@@ -9,4 +11,8 @@ public interface EntityToDtoMapper {
     FlowerDTO toFlowerDTO(Flower flower);
 
     Flower toFlower(FlowerDTO flowerDTO);
+
+    DeliveryType toDeliveryType(DeliveryTypeDTO deliveryTypeDTO);
+
+    DeliveryTypeDTO toDeliveryTypeDTO(DeliveryType deliveryType);
 }
