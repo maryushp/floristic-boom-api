1c3ef44af1dcfc5f625ac3501ed77514d19d2ff6
vburmus <burmusvolod@gmail.com>
2023-12-23 16:17:35 +0100

    fix(config) fixed validation and added object mapper

--- floristicboom/src/main/java/com/floristicboom/flower/model/FlowerDTO.java
+++ floristicboom/src/main/java/com/floristicboom/flower/model/FlowerDTO.java
@@ -13,14 +13,19 @@ public record FlowerDTO(
         Long id,
         @Size(max = 45, message = "{name.size.max}")
         @NotBlank(message = "{name.notBlank}")
+        @NotNull(message = "{name.notNull}")
         String name,
         @Size(max = 255, message = "{description.size.max}")
         @NotBlank(message = "{description.notBlank}")
+        @NotNull(message = "{description.notNull}")
         String description,
         @Positive(message = "{price.positive}")
+        @NotNull(message = "{price.notNull}")
         BigDecimal price,
+        @NotNull(message = "{color.notNull}")
         Color color,
         @PositiveOrZero(message = "{availableQuantity.positiveOrZero}")
+        @NotNull(message = "{availableQuantity.notNull}")
         Integer availableQuantity,
         String imageUri) {
 }
