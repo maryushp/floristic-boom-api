42acb931c7af6ea73a2f9174720831673d8a959e
vburmus <burmusvolod@gmail.com>
2023-12-23 11:57:55 +0100

    feat(floristic-boom) initialized the project

---
+++ floristicboom/src/main/java/com/floristicboom/FloristicBoomApplication.java
@@ -0,0 +1,11 @@
+package com.floristicboom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloristicBoomApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FloristicBoomApplication.class, args);
+    }
+}
