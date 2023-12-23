b3fa7ae992bcb06fd1b0c55bbc26cffc92be2fc3
vburmus <burmusvolod@gmail.com>
2023-12-23 16:20:39 +0100

    feat(config) added crud operations for flower and versioned api

--- floristicboom/src/main/java/com/floristicboom/flower/controller/FlowerController.java
+++ floristicboom/src/main/java/com/floristicboom/flower/controller/FlowerController.java
@@ -2,23 +2,39 @@ package com.floristicboom.flower.controller;
 
 import com.floristicboom.flower.model.FlowerDTO;
 import com.floristicboom.flower.service.FlowerService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/flower")
+@RequestMapping("/api/v1/flower")
 @RequiredArgsConstructor
 public class FlowerController {
     private final FlowerService flowerService;
 
+    @PostMapping
+    public ResponseEntity<FlowerDTO> create(@RequestBody @Valid FlowerDTO flowerDTO) {
+        return ResponseEntity.ok(flowerService.create(flowerDTO));
+    }
+
     @GetMapping
-    public ResponseEntity<Page<FlowerDTO>> readAll(@PageableDefault Pageable pageable){
+    public ResponseEntity<Page<FlowerDTO>> readAll(@PageableDefault Pageable pageable) {
         return ResponseEntity.ok(flowerService.getAll(pageable));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<FlowerDTO> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(flowerService.findById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        flowerService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- floristicboom/src/main/java/com/floristicboom/flower/service/FlowerService.java
+++ floristicboom/src/main/java/com/floristicboom/flower/service/FlowerService.java
@@ -7,4 +7,10 @@ import org.springframework.data.domain.Pageable;
 
 public interface FlowerService {
     Page<FlowerDTO> getAll(Pageable pageable);
+
+    FlowerDTO findById(Long id);
+
+    FlowerDTO create(FlowerDTO flowerDTO);
+
+    void delete(Long id);
 }
