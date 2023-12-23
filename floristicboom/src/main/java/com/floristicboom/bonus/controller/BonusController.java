957aeea2c409cfe51efe293be4b697f3262b81ef
MfalconP <maryushp@gmail.com>
2023-12-23 19:47:04 +0100

    feat(bonus) provided bonus controller

---
+++ floristicboom/src/main/java/com/floristicboom/bonus/controller/BonusController.java
@@ -0,0 +1,45 @@
+package com.floristicboom.bonus.controller;
+
+import com.floristicboom.bonus.model.BonusDTO;
+import com.floristicboom.bonus.service.BonusService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/api/v1/bonus")
+@RequiredArgsConstructor
+public class BonusController {
+    private final BonusService bonusService;
+
+    @PostMapping
+    public ResponseEntity<BonusDTO> create (@RequestBody @Valid BonusDTO bonusDTO) {
+        return ResponseEntity.ok(bonusService.create(bonusDTO));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<BonusDTO>> readAll (@PageableDefault Pageable pageable) {
+        return ResponseEntity.ok(bonusService.readAll(pageable));
+    }
+
+    @GetMapping("/code")
+    public ResponseEntity<BonusDTO> findByPromoCode (@RequestParam("promo_code") String promoCode) {
+        return ResponseEntity.ok(bonusService.findByPromoCode(promoCode));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BonusDTO> findById (@PathVariable Long id) {
+        return ResponseEntity.ok(bonusService.findById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete (@PathVariable Long id) {
+        bonusService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
