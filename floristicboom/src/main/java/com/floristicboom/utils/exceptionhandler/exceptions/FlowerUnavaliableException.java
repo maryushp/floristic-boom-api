7cc0a6958da0ecbfebc37b94e57af02cdbbedd09
vburmus <burmusvolod@gmail.com>
2023-12-26 19:14:25 +0100

    feat(bouquet) updated exception handler and added new FlowerUnavaliableException

---
+++ floristicboom/src/main/java/com/floristicboom/utils/exceptionhandler/exceptions/FlowerUnavaliableException.java
@@ -0,0 +1,7 @@
+package com.floristicboom.utils.exceptionhandler.exceptions;
+
+public class FlowerUnavaliableException extends RuntimeException {
+    public FlowerUnavaliableException(String message) {
+        super(message);
+    }
+}
