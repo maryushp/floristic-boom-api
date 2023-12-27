c3fb32f60b7b3ce1252108a311a078051b848f33
vburmus <burmusvolod@gmail.com>
2023-12-27 11:14:20 +0100

    feat(bouquet) created bouquet service and updated mapper

--- floristicboom/src/main/java/com/floristicboom/utils/mappers/EntityToDtoMapper.java
+++ floristicboom/src/main/java/com/floristicboom/utils/mappers/EntityToDtoMapper.java
@@ -1,15 +1,19 @@
 package com.floristicboom.utils.mappers;
 
+import com.floristicboom.auth.models.RegisterRequest;
 import com.floristicboom.bonus.model.Bonus;
 import com.floristicboom.bonus.model.BonusDTO;
+import com.floristicboom.bouquet.model.Bouquet;
+import com.floristicboom.bouquet.model.BouquetCreationRequest;
+import com.floristicboom.bouquet.model.BouquetDTO;
 import com.floristicboom.delivery.type.model.DeliveryType;
 import com.floristicboom.delivery.type.model.DeliveryTypeDTO;
-import com.floristicboom.auth.models.RegisterRequest;
 import com.floristicboom.flower.model.Flower;
 import com.floristicboom.flower.model.FlowerDTO;
 import com.floristicboom.user.model.User;
 import com.floristicboom.user.model.UserDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface EntityToDtoMapper {
@@ -28,4 +32,9 @@ public interface EntityToDtoMapper {
     UserDTO toUserDTO(User user);
 
     User toUser(RegisterRequest registerRequest);
+
+    BouquetDTO toBouquetDTO(Bouquet bouquet);
+
+    @Mapping(target = "flowers", ignore = true)
+    Bouquet toBouquet(BouquetCreationRequest bouquetCreationRequest);
 }
