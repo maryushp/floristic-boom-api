4cf8705a638c6aa5b2dcb37b47b90418615f0a07
vburmus <burmusvolod@gmail.com>
2024-01-03 14:58:27 +0100

    fix(auth) changed service dependency to repository

--- floristicboom/src/main/java/com/floristicboom/credentials/service/CustomUserDetailsService.java
+++ floristicboom/src/main/java/com/floristicboom/credentials/service/CustomUserDetailsService.java
@@ -1,18 +1,21 @@
 package com.floristicboom.credentials.service;
 
 import com.floristicboom.credentials.model.Credentials;
+import com.floristicboom.credentials.repository.CredentialsRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import static com.floristicboom.utils.Constants.USER_WITH_LOGIN_NOT_FOUND;
+
 @Service
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
-    private final CredentialsService credentialsService;
+    private final CredentialsRepository credentialsRepository;
 
     @Override
     public Credentials loadUserByUsername(String username) throws UsernameNotFoundException {
-        return credentialsService.findByLogin(username);
+        return credentialsRepository.findByLogin(username).orElseThrow(() -> new UsernameNotFoundException(String.format(USER_WITH_LOGIN_NOT_FOUND, username)));
     }
 }
