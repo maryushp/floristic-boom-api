3bb1a31ace96907abadcbcff662ba966e2565aef
maryushp <maryushp@gmail.com>
2024-01-13 21:14:04 +0100

    feat(flower filtering) provided repository method

--- floristicboom/src/main/java/com/floristicboom/flower/repository/FlowerRepository.java
+++ floristicboom/src/main/java/com/floristicboom/flower/repository/FlowerRepository.java
@@ -2,9 +2,14 @@ package com.floristicboom.flower.repository;
 
 
 import com.floristicboom.flower.model.Flower;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FlowerRepository extends JpaRepository<Flower, Long> {
+
+    Page<Flower> findAll(Specification<Flower> spec, Pageable pageable);
 }
